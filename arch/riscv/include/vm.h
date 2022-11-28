0419987c8671473c3563bc7cb8c2303144fa66dd
ej <e.j.hangzhou@icloud.com>
2022-11-28 17:21:23 +0800

    virtual memory

--- arch/riscv/include/vm.h
+++ arch/riscv/include/vm.h
@@ -5,6 +5,11 @@
 
 #define offset (0xffffffe000000000 - 0x80000000)
 
+#define P_KERNEL 0x80000000
+#define V_KERNEL 0xffffffe000000000
+#define MAP_SIZE 0x1000000 //16MB
+#define UART_ADDR 0x10000000
+
 void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
                     int perm);
 void paging_init();
--- arch/riscv/kernel/vm.c
+++ arch/riscv/kernel/vm.c
@@ -1,12 +1,20 @@
 #include "vm.h"
 #include "put.h"
 
+#define PAGE_SIZE 0x1000 //4kb
+#define PAGE_FAULT -1
+#define RWX 7
+
 extern unsigned long long text_start;
 extern unsigned long long rodata_start;
 extern unsigned long long data_start;
 extern unsigned long long _end;
 
 #define offset (0xffffffe000000000 - 0x80000000)
+
+int page_count = 0;
+
+uint64_t* trace_pte(uint64_t* pgtbl,uint64_t va);
 
 void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
                     int perm) {
@@ -28,10 +36,105 @@ void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
     PPN[1]: 9 bits
     PPN[0]: 9 bits
    */
-  uint64_t address_max;
+  uint64_t va_end;
+  va_end = va + sz -1;
+
+  for (1; va<= va_end ; va += PAGE_SIZE, pa+=PAGE_SIZE){
+    uint64_t * pte_address;
+
+    pte_address = trace_pte(pgtbl, va);
+    if(pte_address != PAGE_FAULT){
+
+      /* get ppn by eliminating offset */
+      uint64_t ppn = pa >> 12;
+
+      /* set pte : reserved  + ppn + perm */
+      *pte_address = 0xffc0000000000000 | (uint64_t)(ppn << 10) | (uint64_t)perm;
+    }
+
+  }
 
 
 
 }
 
-void paging_init() { /* your code */ }
+
+
+/*   
+        RISC-V Sv39 Virtual Address and Physical Address Translation
+     38        30 29        21 20        12 11                           0
+     ---------------------------------------------------------------------
+    |   VPN[2]   |   VPN[1]   |   VPN[0]   |          page offset         |
+     ---------------------------------------------------------------------
+                            Sv39 virtual address */
+uint64_t* trace_pte(uint64_t* pgtbl,uint64_t va){
+  /* pgtbl: 根页表基地址
+     va: 虚拟地址
+   */
+  uint64_t* pte_address;
+
+  for(int i = 2; i>=0 ;i--){
+    uint64_t pte;
+    if(i==2){
+      uint64_t vpn_2 = (va >> 30) & 0x1ff;
+      pte_address = (uint64_t)(pgtbl + vpn_2);
+    }
+    if(i==1){
+      uint64_t vpn_1 = (va >> 21) & 0x1ff;
+      pte_address = (uint64_t)(pgtbl + vpn_1);
+    }
+    if(i==0){
+      uint64_t vpn_0 = (va >> 12) & 0x1ff;
+      pte_address = (uint64_t)(pgtbl + vpn_0);
+    }
+    
+    /* if pte is valid */
+    if( (*pte_address & 0x1) ){
+      /* get ppn by eliminating offset */
+      uint64_t ppn = (*pte_address) >> 10;
+
+      /* get next level pgtbl */
+      pgtbl = (uint64_t*)(ppn << 12);
+    }
+
+    else{
+      page_count ++; 
+      pgtbl = (uint64_t)(&_end + PAGE_SIZE * page_count);
+      uint64_t ppn = ((uint64_t)pgtbl >> 12)&(0xfffffffffff);
+      *pte_address = ((uint64_t)(*pte_address) & 0xffc0000000000000) | (ppn << 10) | 0x1 ;
+    }
+  }
+
+  return pte_address;
+
+
+
+}
+
+
+void paging_init() { 
+  uint64_t *pgtbl = &_end;
+
+  /* 映射内核起始的16MB空间到高地址 */
+
+  /*kernel起始的16mb映射到高地址*/
+  create_mapping(pgtbl, (uint64_t)V_KERNEL, (uint64_t)P_KERNEL, (uint64_t)MAP_SIZE, RWX);
+  /*kernel起始地址的16mb做等值映射*/
+  create_mapping(pgtbl,(uint64_t)P_KERNEL, (uint64_t)P_KERNEL, (uint64_t)MAP_SIZE, RWX);
+  /*UART等值映射*/
+  create_mapping(pgtbl, (uint64_t)UART_ADDR, (uint64_t)UART_ADDR, (uint64_t)MAP_SIZE, RWX);
+
+
+
+
+
+  
+  // create_mapping(pgtbl, (uint64_t)(&text_start + offset), (uint64_t)(&text_start), 0x2000, RWX);
+
+  // create_mapping(pgtbl, (uint64_t)(&rodata_start + offset), (uint64_t)(&rodata_start), 0x2000, RWX);
+
+  // create_mapping(pgtbl, (uint64_t)(&text_start + offset), (uint64_t)(&text_start), 0x2000, RWX);
+
+  // create_mapping(pgtbl, (uint64_t)(&text_start + offset), (uint64_t)(&text_start), 0x2000, RWX);
+
+}
