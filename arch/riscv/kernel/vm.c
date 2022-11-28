d060ad4aea9f6fe91ad4b20bf33d2cda37372123
ej <e.j.hangzhou@icloud.com>
2022-11-28 16:31:06 +0800

    working on vm.c

--- arch/riscv/kernel/vm.c
+++ arch/riscv/kernel/vm.c
@@ -10,7 +10,28 @@ extern unsigned long long _end;
 
 void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
                     int perm) {
-  /* your code */
+  /* pgtbl: 根页表基地址
+     va: 虚拟地址
+     pa: 物理地址
+     sz: 映射大小
+     perm: 权限
+   */
+
+  /* pte : 
+   63      54 36        28 27        19 18        10 9   8 7 6 5 4 3 2 1 0
+   -----------------------------------------------------------------------
+  | Reserved |   PPN[2]   |   PPN[1]   |   PPN[0]   | RSW |D|A|G|U|X|W|R|V| 
+   -----------------------------------------------------------------------
+
+    Reserved: 全1
+    PPN[2]: 9 bits
+    PPN[1]: 9 bits
+    PPN[0]: 9 bits
+   */
+  uint64_t address_max;
+
+
+
 }
 
 void paging_init() { /* your code */ }
