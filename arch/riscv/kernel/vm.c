4c4eaa21ba1c16561a42da36ebaa79ae9ab77ef8
Left <3200102523@zju.edu.cn>
2022-11-30 11:22:19 +0800

    sched.c

--- arch/riscv/kernel/vm.c
+++ arch/riscv/kernel/vm.c
@@ -131,11 +131,6 @@ void paging_init() {
   create_mapping(pgtbl,(uint64_t)P_KERNEL, (uint64_t)P_KERNEL, (uint64_t)MAP_SIZE, RWX);
   /*UART等值映射*/
   create_mapping(pgtbl, (uint64_t)UART_ADDR, (uint64_t)UART_ADDR, (uint64_t)MAP_SIZE, RWX);
-
-
-
-
-
   
   // create_mapping(pgtbl, (uint64_t)(&text_start + offset), (uint64_t)(&text_start), 0x2000, RWX);
 
@@ -149,32 +144,32 @@ void paging_init() {
 
 
 /* js implemented */
-void paging_init() { 
-  /* your code */
-  //设置映射
-  uint64_t *pgtbl = &_end;
-  uint64_t pa_start = 0x80000000;
-  uint64_t va_start = 0xffffffe000000000;
-  uint64_t UART = 0x10000000;
-  uint64_t size = 0x1000000;
-  uint64_t text_size = 0x2000;
-  uint64_t rodata_size = 0x1000;
-  uint64_t data_size = size - text_size - rodata_size;
+// void paging_init() { 
+//   /* your code */
+//   //设置映射
+//   uint64_t *pgtbl = &_end;
+//   uint64_t pa_start = 0x80000000;
+//   uint64_t va_start = 0xffffffe000000000;
+//   uint64_t UART = 0x10000000;
+//   uint64_t size = 0x1000000;
+//   uint64_t text_size = 0x2000;
+//   uint64_t rodata_size = 0x1000;
+//   uint64_t data_size = size - text_size - rodata_size;
 
-  int perm = 7;
-  create_mapping(pgtbl, va_start, pa_start, size, perm);  //高地址映射
-  create_mapping(pgtbl, pa_start, pa_start, size, perm);  //kernel等值映射
-  create_mapping(pgtbl, UART, UART, size, perm);          //UART等值映射
+//   int perm = 7;
+//   create_mapping(pgtbl, va_start, pa_start, size, perm);  //高地址映射
+//   create_mapping(pgtbl, pa_start, pa_start, size, perm);  //kernel等值映射
+//   create_mapping(pgtbl, UART, UART, size, perm);          //UART等值映射
 
-  //权限保护
-  //高地址映射
-  create_mapping(pgtbl, text_start+offset, text_start, text_size, 5);  //text XWR = 101
-  create_mapping(pgtbl, rodata_start+offset, rodata_start, rodata_size, 1);  //rodata XWR = 001
-  create_mapping(pgtbl, data_start+offset, data_start, data_size, 3);  //data XWR = 011
-  //kernel等值映射
-  create_mapping(pgtbl, text_start, text_start, text_size, 5);  //text XWR = 101
-  create_mapping(pgtbl, rodata_start, rodata_start, rodata_size, 1);  //rodata XWR = 001
-  create_mapping(pgtbl, data_start, data_start, data_size, 3);  //data XWR = 011
-  //UART等值映射
-  create_mapping(pgtbl, UART, UART, size, 3);  //data XWR = 011        
-}
+//   //权限保护
+//   //高地址映射
+//   create_mapping(pgtbl, text_start+offset, text_start, text_size, 5);  //text XWR = 101
+//   create_mapping(pgtbl, rodata_start+offset, rodata_start, rodata_size, 1);  //rodata XWR = 001
+//   create_mapping(pgtbl, data_start+offset, data_start, data_size, 3);  //data XWR = 011
+//   //kernel等值映射
+//   create_mapping(pgtbl, text_start, text_start, text_size, 5);  //text XWR = 101
+//   create_mapping(pgtbl, rodata_start, rodata_start, rodata_size, 1);  //rodata XWR = 001
+//   create_mapping(pgtbl, data_start, data_start, data_size, 3);  //data XWR = 011
+//   //UART等值映射
+//   create_mapping(pgtbl, UART, UART, size, 3);  //data XWR = 011        
+// }
